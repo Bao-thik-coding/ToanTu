0cae0e98341dfb964e0e14b5f0321aa61866ce2f
Bao-thik-coding <113531784+Bao-thik-coding@users.noreply.github.com>
2023-07-01 20:37:49 +0700

    ds

---
+++ ToanTu/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToanTu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cao;
+            int dai;
+
+            Console.Write("Nhap do dai: ");
+            dai = int.Parse(Console.ReadLine());
+            Console.Write("Nhap do cao: ");
+            cao = int.Parse(Console.ReadLine());
+
+            int dien_tich = cao * dai;
+            Console.WriteLine("Dien Tich la " + dien_tich);
+            Console.ReadKey();
+        }
+    }
+}
